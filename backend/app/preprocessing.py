44cc217f4d34470565f2ca9016d7e4c8aecbb1da
TarekSaati <tarek.saati@hiast.edu.sy>
2023-07-02 16:28:47 +0300

    fixed FunctionTransformer

--- backend/app/preprocessing.py
+++ backend/app/preprocessing.py
@@ -1,7 +1,6 @@
 
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import FunctionTransformer
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler, FunctionTransformer
 import pandas as pd
 import numpy as np
 import json
